202a8d6a554b640a37e4ab6861fe78b3266ffa8f
Paulo Matos <paulomatos4554@gmail.com>
2022-06-17 10:44:25 -0300

    jsx manipulating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Matheus";
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>olá React</h1>
+      <h1>Meu primeiro App</h1>
+      <p>OLá, {newName}</p>
+      <p>Soma:{sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
