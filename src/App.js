932a4d13861deed476d4e63390f740e611bde403
Paulo Matos <paulomatos4554@gmail.com>
2022-06-20 14:58:52 -0300

    list render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Condition from "./components/Condition";
+import OtherLIst from "./components/OtherList";
 
 function App() {
+  const lista1 = ["React", "Vue", "Angular"];
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condition />
+      <h1>Renderização de Listas</h1>
+      <OtherLIst itens={lista1} />
+      <OtherLIst itens={[]} />
     </div>
   );
 }
---
+++ src/components/OtherList.js
@@ -0,0 +1,13 @@
+function OtherLIst({ itens }) {
+  return (
+    <>
+      <h3>Lista de coisas boas:</h3>
+      {itens.length > 0 ? (
+        itens.map((item, index) => <p key={index}>{item}</p>)
+      ) : (
+        <p>Lista vazia</p>
+      )}
+    </>
+  );
+}
+export default OtherLIst;
